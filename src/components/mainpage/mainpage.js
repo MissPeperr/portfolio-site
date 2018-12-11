d479591e16817951d6763bac8b3ac41843d1abd8
Madi Peper <madisonpeper@gmail.com>
2018-12-10 19:39:49 -0600

    added fun photo of myself, and actual descriptions of each area

--- src/components/mainpage/mainpage.js
+++ src/components/mainpage/mainpage.js
@@ -20,19 +20,26 @@ export default class MainPage extends Component {
                 <div id="about-me">
                 <br></br>
                     <h2>Who am I?</h2>
-                    <p> Attack feet groom forever, stretch tongue and leave it slightly out, blep and cat not kitten around so hide at bottom of staircase to trip human and purr when being pet so sleep on keyboard. Walk on car leaving trail of paw prints on hood and windshield. Kitten is playing with dead mouse sleep on my human's head but if it fits, i sits eat a plant, kill a hand. Purr while eating kitty scratches couch bad kitty, so attempt to leap between furniture but woefully miscalibrate and bellyflop onto the floor; what's your problem? i meant to do that now i shall wash myself intently. Allways wanting food cats making all the muffins. Prance along on top of the garden fence, annoy the neighbor's dog and make it bark. Sun bathe. Purr for no reason. Rub face on owner stare at the wall, play with food and get confused by dust fall asleep upside-down play riveting piece on synthesizer keyboard. Meow in empty rooms hack.</p>
+                    <img alt="Madison Peper" src="MadisonPeper_Fun.jpg"></img>
+                    <p>I'm a problem solving nerd on a mission to learn as much as I can in my lifetime. I've always expressed my creativity through art across various different mediums, and I've found that I can express my creativity through code as well. While working in the customer service industry, I encountered a world of problems that needed solving. Seeing the results of my work in my customer's happy faces and positive feedback, I realized that I not only enjoyed problem solving, but I had a knack for it. That's when I looked to software development.
+                    Born and raised from the Sacramento area of California.
+                    </p>
                 </div>
                 <div id="parallax-space"></div>
                 <div id="project-div">
                 <br></br>
                     <h2>What have I worked on?</h2>
                     <div id="collectable-div">
-                        <h4>CollectAble</h4>
-                        <p>DON'T FORGET TO ADD A LINK</p>
-                        <p>Attack feet groom forever, stretch tongue and leave it slightly out, blep and cat not kitten around so hide at bottom of staircase to trip human and purr when being pet so sleep on keyboard. Walk on car leaving trail of paw prints on hood and windshield. Kitten is playing with dead mouse sleep on my human's head but if it fits, i sits eat a plant, kill a hand. Purr while eating kitty scratches couch bad kitty, so attempt to leap between furniture but woefully miscalibrate and bellyflop onto the floor; what's your problem? i meant to do that now i shall wash myself intently. Allways wanting food cats making all the muffins. Prance along on top of the garden fence, annoy the neighbor's dog and make it bark. Sun bathe. Purr for no reason. Rub face on owner stare at the wall, play with food and get confused by dust fall asleep upside-down play riveting piece on synthesizer keyboard. Meow in empty rooms hack.</p>
-                        <h4>2nd Project Goes Here!</h4>
-                        <p>DON'T FORGET TO ADD A LINK</p>
-                        <p>Attack feet groom forever, stretch tongue and leave it slightly out, blep and cat not kitten around so hide at bottom of staircase to trip human and purr when being pet so sleep on keyboard. Walk on car leaving trail of paw prints on hood and windshield. Kitten is playing with dead mouse sleep on my human's head but if it fits, i sits eat a plant, kill a hand. Purr while eating kitty scratches couch bad kitty, so attempt to leap between furniture but woefully miscalibrate and bellyflop onto the floor; what's your problem? i meant to do that now i shall wash myself intently. Allways wanting food cats making all the muffins. Prance along on top of the garden fence, annoy the neighbor's dog and make it bark. Sun bathe. Purr for no reason. Rub face on owner stare at the wall, play with food and get confused by dust fall asleep upside-down play riveting piece on synthesizer keyboard. Meow in empty rooms hack.</p>
+                        <a href="https://collect-able-app.herokuapp.com" style={{fontSize: "2em", color: "black"}}>CollectAble</a>
+                        <br></br>
+                        <a href="https://github.com/MissPeperr/CollectAble" style={{color: "darkblue"}}>Github</a>
+                        <p>CollectAble is a minimalistic, front-end application used for keeping track of your personal collections. You can create multiple collections, each with their own list of collectables. While creating a collectable, you'll be prompted to add a name, a short description, a photo, and a price. The price isn't necessary, but is included incase you like to trade or sell your collectables! Made with React and a JSON server.</p>
+                    </div>
+                    <div id="dps-div">
+                        <a style={{fontSize: "2em", color: "black"}}>Donation Pick Up Services</a>
+                        <br></br>
+                        <a style={{color: "darkblue"}}>Github</a>
+                        <p>Donation Pick Up Services is an application to schedule  a pick up service for your donations. As a Customer, you can create a donation, view a history of your donations, and check the status of your donation. As an Employee, you can change the status of donations so customers are aware of its progress, and create and manage the types of donations. Coded in C# with .NET Entity Framework.</p>
                     </div>
                 </div>
                 <div id="parallax-nash"></div>
